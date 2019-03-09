cb14e075936ecaa9e9b74ffd6b7d351a37cba792
Pawel Matusiak <pawel.matusiak29@gmail.com>
2019-03-09 12:19:57 +0100

    Person

---
+++ src/main/java/com/sda/json/Main.java
@@ -0,0 +1,13 @@
+package com.sda.json;
+
+/**
+ * @author pmatusiak
+ */
+public class Main {
+    Person person1 = new Person("Paweł", "Matusiak", 28);
+    Person person2 = new Person("Remi", "Jakiśtam", 25);
+
+    public static void main(String[] args) {
+
+    }
+}
